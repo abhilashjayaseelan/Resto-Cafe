b675d819007a5daba5c966b6a521c4215bd68a46
Abhilash MJ <abhialash.jayaseelan@gmail.com>
2023-08-24 10:52:48 +0530

    error page added

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Restaurants } from "./App";
+import NotFound from "./components/ErrorPage";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <Restaurants />,
-    children: [
-      {
-        path: "/",
-        element: <Restaurants />,
-      },
-    ],
+    errorElement: <NotFound/>
   },
 ]);
 
